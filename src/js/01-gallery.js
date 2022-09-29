5978b3df02e026a124c82bfb2d3b0d5ffed5f003
KovalchukAnastasia <103355891+KovalchukAnastasia@users.noreply.github.com>
2022-09-29 12:35:46 +0300

    add SsimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,8 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox"
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports go this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// console.log(SimpleLightbox);
 console.log(galleryItems);
